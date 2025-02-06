d48508a660d815a91389dc5f284da2e2ea4d7df5
KimigaiiWuyi <444835641@qq.com>
2025-02-07 02:30:36 +0800

    🎨 报错提示

--- BlueArchiveUID/utils/download.py
+++ BlueArchiveUID/utils/download.py
@@ -14,7 +14,7 @@ async def download_file(
     path: Path,
     name: str,
     size: Optional[Tuple[int, int]] = None,
-) -> Union[Image.Image, Tuple[str, Path, str], None]:
+) -> Union[Image.Image, str, None]:
     file_path = path / name
     if file_path.exists():
         if size:
@@ -25,16 +25,26 @@ async def download_file(
         try:
             logger.info(f'[ba]开始下载: {name} | 地址: {url}')
             async with sess.get(url) as res:
-                content = await res.read()
-                logger.info(f'[ba]下载成功: {name}')
+                if res.status == 200:
+                    content = await res.read()
+                    logger.info(f'[ba] 下载成功: {name}')
+                else:
+                    logger.warning(
+                        f"[ba] {name}下载失败! 状态码: {res.status}！"
+                    )
+                    return f'下载失败! 状态码: {res.status}！'
         except ClientConnectorError:
             logger.warning(f"[ba]{name}下载失败")
-            return url, path, name
+            return '下载失败! 链接状态错误!'
 
     async with aiofiles.open(path / name, "wb") as f:
         await f.write(content)
         stream = BytesIO(content)
-        if size:
-            return Image.open(stream).resize(size)
-        else:
-            return Image.open(stream)
+        try:
+            if size:
+                return Image.open(stream).resize(size)
+            else:
+                return Image.open(stream)
+        except OSError:
+            logger.warning(f"[ba] {name} 下载失败! 图片损坏!")
+            return '图片损坏...可能是命令错误！'
